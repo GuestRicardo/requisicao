49c79197344194e3e7d2889b95cfc78d7222ea0a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-11-25 17:41:05 -0300

    formatando

--- requisicao_web/lib/main.dart
+++ requisicao_web/lib/main.dart
@@ -2,5 +2,11 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final future = http.get(Uri.http('www.example.com'));
+  final future = http.get(Uri.https('www.example.com', '/'));
+
+  future.then((response) {
+    if (response.statusCode == 200) {
+      print('Pagina carregada com sucesso(codigo 200 é pagina em ok)');
+    }
+  });
 }
